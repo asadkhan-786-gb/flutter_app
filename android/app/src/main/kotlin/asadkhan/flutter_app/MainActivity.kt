2e134a2a5f40e20fed218fc2bcd293a9e2b9bc5a
Muhammad Asad Khan <m.asadkhan079@gmail.com>
2021-05-14 02:57:43 +0500

    first commit

---
+++ android/app/src/main/kotlin/asadkhan/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package asadkhan.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Asad Khan m.asadkhan079@gmail.com'),
+      ),
+    ),
+  ));
+}
+
